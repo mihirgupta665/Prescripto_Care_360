5abef9853e9f7e2a33888ebe504c172826b446b8
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-14 15:10:05 +0530

    DropDown menu created from profile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,6 +37,14 @@ const Navbar = () => {
                         {/* try to add user personalized picture */}
                         <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
                         <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+                        {/* group hover makes it visible as block when its group is hovered */}
+                        <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+                            <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                                <p>My Profile</p>
+                                <p>My Appointments</p>
+                                <p>Logout</p>
+                            </div>
+                        </div>
                     </div>
                     : <button onClick={() => { navigate("/login") }} className="bg-primary text-white font-light px-8 py-3 rounded-full hidden md:block">CREATE ACCOUNT</button>
                 }
