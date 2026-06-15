792dfc979ab1bb452807100223326d6a1d447440
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-15 15:30:49 +0530

    doctor info extracted and processed from doc id in appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -11,15 +11,21 @@ const Appointment = () => {
     const fetchDocInfo = async () => {
         const docInfo = doctors.find(doc => doc._id===docId)
         setDocInfo(docInfo)
+        console.log(docInfo)
     }
 
     useEffect( () => {
-
+        fetchDocInfo()
     }, [doctors, docId] )
 
     return(
         <div>
-
+            {/* ----- Doctor Details */}
+            <div>
+                <div>
+                    <img src={docInfo.image} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
