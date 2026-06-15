74b93634e04936eb33d87cbfe5e37c882aae24ab
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-15 18:00:33 +0530

    Getting Current Data functionality written

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -7,17 +7,41 @@ const Appointment = () => {
 
     const {docId} = useParams()
     const {doctors, currencySymbol} = useContext(AppContext)
+
     const [docInfo, setDocInfo] = useState(null)
+    const [docSlots, setDocSlots] = useState([])
+    const [slotIndex, setSlotIndex] = useState(0)
+    const [slotTime, setSlotTime] = useState("")
+
 
     const fetchDocInfo = async () => {
         const docInfo = doctors.find(doc => doc._id===docId)
         setDocInfo(docInfo)
-        console.log(docInfo)
+        // console.log(docInfo)
+    }
+
+    const getAvailableSlots = async () => {
+        setDocSlots([])
+
+        // getting current date
+        let today = new Date()
+        for(let i=0; i<7; i++){
+            // getting future dates
+            let currentDate = new Date(today)  // what is actual value in today and all changing value in currentDate could not we write directly write currentDate = today+i
+            currentDate.setDate(today.getDate() + i)  // what is the value of today and today.getDate()
+
+            // setting end time of the date with index
+        }
+
     }
 
     useEffect( () => {
         fetchDocInfo()
     }, [doctors, docId] )
+
+    useEffect( () => {
+        getAvailableSlots()
+    }, [docInfo] )
 
     return docInfo && (
         <div>
