4d9f9afdeebaa16c49a9595e796ebaf7ffeb238a
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-16 19:03:17 +0530

    My Profile Page Created

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -21,37 +21,37 @@ const MyProfile = () => {
     return (
         <div className="max-w-lg flex flex-col gap-2 text-sm">
 
-            <img src={userData.image} alt="" />
+            <img className="w-36 rounded" src={userData.image} alt="" />
 
             {
                 isEdit
-                    ? <input type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} />
-                    : <p>{userData.name}</p>
+                    ? <input className="bg-gray-100 text-3xl font-medium max-w-60 mt-4" type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} />
+                    : <p className="font-medium text-3xl text-neutral-800 mt-4" >{userData.name}</p>
             }
 
-            <hr />
+            <hr className="bg-zinc-400 h-[1px] border-none"/>
             <div>
-                <p>CONTACT INFORMATION</p>
-                <div>
-                    <p>Email ID: </p>
-                    <p>{userData.email}</p>
+                <p className="text-neutral-500 underline mt-3">CONTACT INFORMATION</p>
+                <div className="grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
+                    <p className="font-medium">Email ID: </p>
+                    <p className="text-blue-500">{userData.email}</p>
 
-                    <p>Phone: </p>
+                    <p className="font-medium">Phone: </p>
                     {
                         isEdit
-                            ? <input type="text" value={userData.phone} onChange={e => setUserData(prev => ({ ...prev, phone: e.target.value }))} />
-                            : <p>{userData.phone}</p>
+                            ? <input className="bg-gray-100 max-w-52" type="text" value={userData.phone} onChange={e => setUserData(prev => ({ ...prev, phone: e.target.value }))} />
+                            : <p className="text-gray-500">{userData.phone}</p>
                     }
 
-                    <p>Address: </p>
+                    <p className="font-medium">Address: </p>
                     {
                         isEdit
                             ? <p>
-                                <input type="text" value={userData.address.line1} onChange={e => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} />
+                                <input className="bg-gray-100" type="text" value={userData.address.line1} onChange={e => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} />
                                 <br />
-                                <input type="text" value={userData.address.line2} onChange={e => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} />
+                                <input className="bg-gray-100" type="text" value={userData.address.line2} onChange={e => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} />
                             </p>
-                            : <p>
+                            : <p className="text-gray-500">
                                 {userData.address.line1}
                                 <br />
                                 {userData.address.line2}
