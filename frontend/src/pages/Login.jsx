fc0b33d4a72a5f6f415ce1a5c0cc53a0b868984b
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-16 15:27:48 +0530

    Login Page Created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
-    
+
     const onSubmitHandler = async (event) => {
         event.preventDefault()
     }
@@ -16,21 +16,23 @@ const Login = () => {
             <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
                 <p className="text-2xl font-semibold">{state === "Sign Up" ? "Create Account" : "Login"}</p>
                 <p>Please {state === "Sign Up" ? "Sign Up" : "Login"} to book your Appointment</p>
-                <div className="w-full">
-                    <p>Full Name</p>
-                    <input  className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" value={name} onChange={ (e) => setName(e.target.value) } required />
-                </div>
+                {state === "Sign Up" &&
+                    <div className="w-full">
+                        <p>Full Name</p>
+                        <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+                    </div>}
+
                 <div className="w-full">
                     <p>Email</p>
-                    <input  className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" value={email} onChange={ (e) => setEmail(e.target.value) } required />
+                    <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 </div>
                 <div className="w-full">
                     <p>Password</p>
-                    <input  className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" value={password} onChange={ (e) => setPassword(e.target.value) } required />
+                    <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 </div>
                 <button className="bg-primary text-white w-full py-2 rounded-md text-base">{state === "Sign Up" ? "Create Account" : "Login"}</button>
                 {
-                    state === "Sign Up" 
+                    state === "Sign Up"
                         ? <p>Already have an Account? &nbsp; <span onClick={() => setState("Login")} className="text-primary underline cursor-pointer"> Login here</span></p>
                         : <p>Create a new Account? &nbsp; <span onClick={() => setState("Sign Up")} className="text-primary underline cursor-pointer"> Click here</span></p>
                 }
