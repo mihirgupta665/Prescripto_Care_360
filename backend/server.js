9027ce02462d85b1cd1b67bc3e36415c98096cc9
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-17 04:21:18 +0530

    Cloudinary Database Configured in the backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import cors from "cors"
 import "dotenv/config"
 
 import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
 
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 
 
