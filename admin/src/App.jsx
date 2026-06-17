3c49063d39bbea6ee47e5dc754bcaa74b742551c
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-18 04:11:45 +0530

    Navbar created for the admin and doctor panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login"
 import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { AdminContext } from "./context/AdminContext"
+import Navbar from "./components/Navbar"
 
 const App = () => {
 
@@ -10,9 +11,10 @@ const App = () => {
 
     return aToken 
     ? (
-        <div>
-            
+        <div className="bg-[#F8F9FD]">
             <ToastContainer />
+            <Navbar />
+            
         </div>
     ) 
     : (
