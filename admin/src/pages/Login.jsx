c5f55340766a75ada55826fa243666ff19c48b77
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-18 01:24:53 +0530

    State and context created for email and password vite env created and fetched with backend url

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { assets } from "../assets/assets"
+import { AdminContext } from "../context/AdminContext"
 
 const Login = () => {
 
     const [state, setState] = useState("Admin")
+    const [email, setEmail] = useState("Admin")
+    const [password, setPassword] = useState("Admin")
+
+    const {setAToken, backendUrl} = useContext(AdminContext)
 
     return (
         <form className="min-h-[80vh] flex items-center">
@@ -11,11 +16,11 @@ const Login = () => {
                 <p className="text-2xl font-semibold m-auto"><span className="text-primary">{state}</span> Login</p>
                 <div className="w-full">
                     <p className="text-black">Email</p>
-                    <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type="email" required />
+                    <input value={email} onClick={e => setEmail(e.target.value)} className="border border-[#DADADA] rounded w-full p-2 mt-1" type="email" required />
                 </div>
                 <div className="w-full">
                     <p className="text-black">Password</p>
-                    <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type="password" required />
+                    <input value={password} onChange={e => setPassword(e.target.value)} className="border border-[#DADADA] rounded w-full p-2 mt-1" type="password" required />
                 </div>
                 <button className="bg-primary text-white w-full py-2 rounded-md text-base">Login</button>
                 {
