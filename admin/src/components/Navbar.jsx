f95fdf63417fa0e7c67790dbe6242749700920ca
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-18 14:43:29 +0530

    Admin logo added correctly and updated with uplad area logo too for better visualization

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
     }
 
     return (
-        <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
+        <div className="flex justify-between items-center px-4 sm:px-10 py-2 border-b bg-white">
             <div className="flex items-center gap-2 text-xs">
-                <img className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo} alt="" />
+                <img className="h-12 sm:h-14 w-auto cursor-pointer" src={assets.admin_logo} alt="" />
                 <p className="border mx-4 px-4 py-0.5 rounded-full border-primary text-green-600 text-sm">{aToken ? "Admin" : "Doctor"}</p>
             </div>
             <button onClick={logout} className="bg-primary text-white text-sm px-10 py-2 rounded-full">Logout</button>
--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
     return (
         <form>
-            
+
+            <p>Add Doctor</p>
+
+            <div>
+                <div>
+                    <label htmlFor="doc-img">
+                        <img className='w-48 rounded-full' src={assets.upload_area} alt="" />
+                    </label>
+                    <input type="file" id="doc-img" hidden/>
+                    <p>Upload doctor <br /> picture</p>
+                </div>
+
+                <div>
+                    <div>
+
+                        <div>
+                            <p>Doctor Name : </p>
+                            <input type="text" placeholder='Name' required/>
+                        </div>
+                        <div>
+                            <p>Doctor Email : </p>
+                            <input type="email" placeholder='Email' required/>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </form>
     )
 }
