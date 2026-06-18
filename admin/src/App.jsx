b8591fa6336355ed0496081d03e930896c475c78
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-18 13:51:17 +0530

    Sidebar created and structured layout

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,9 @@ import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import { Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Admin/Dashboard"
+import AllAppointments from "./pages/Admin/AllAppointments"
+import AddDoctor from "./pages/Admin/AddDoctor"
+import DoctorsList from "./pages/Admin/DoctorsList"
 
 const App = () => {
 
@@ -22,6 +25,9 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<></>} />
                     <Route path="/admin-dashboard" element={<Dashboard />} />
+                    <Route path="/all-appointments" element={<AllAppointments />} />
+                    <Route path="/add-doctor" element={<AddDoctor />} />
+                    <Route path="/doctor-list" element={<DoctorsList />} />
                 </Routes>
             </div>
             
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+
+    const { aToken } = useContext(AdminContext)
+
+    return (
+        <div>
+            {
+                aToken && 
+                <ul>
+
+                    <NavLink to={"/admin-dashboard"}>
+                        <img src={assets.home_icon} alt="" />
+                        <p>Dashboard</p>
+                    </NavLink>
+                    <NavLink to={"/all-appointments"}>
+                        <img src={assets.appointment_icon} alt="" />
+                        <p>Appoointments</p>
+                    </NavLink>
+                    <NavLink to={"/add-doctor"}>
+                        <img src={assets.add_icon} alt="" />
+                        <p>Add Doctors</p>
+                    </NavLink>
+                    <NavLink to={"/doctor-list"}>
+                        <img src={assets.people_icon} alt="" />
+                        <p>Doctors List</p>
+                    </NavLink>
+
+                </ul>
+            }
+        </div>
+    )
 }
 
 export default Sidebar
