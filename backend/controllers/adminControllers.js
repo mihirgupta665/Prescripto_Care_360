721458956e56d0beccac04143b02cb66e949f95e
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-22 03:38:25 +0530

    Created the controller function to get all the appointments of all the doctors for the admin panel

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt"
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
 import jwt from "jsonwebtoken"
+import appointmentModel from "../models/appointmentModel.js"
 
 
 const addDoctor = async (req, res) => {
@@ -99,6 +100,28 @@ const allDoctors = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+
+// API to get all appointment list
+const appointmentsAdmin = async (req, res) => {
+
+    try {
+    
+        const appointments = await appointmentModel.find({})
+        res.json({success:true, appointments})
+
+
+    }
+    catch (error) {
+        
+        console.log("Error Occured during fetching all the appointments from the database. Error : ",error)
+        res.json({success:false, message:error.message})
+
+    }
+
+
+}
+
 
 export default addDoctor
 export { loginAdmin, allDoctors }
