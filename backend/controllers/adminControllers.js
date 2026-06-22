697641a0d04e9b9364bfa1a5bfbca2a6da16fb17
Mihir Gupta <mihirgupta665@gmail.com>
2026-06-22 14:58:39 +0530

    Appointment Cancellation controller function created for Admin to faciliate the cancelation of a desired Appointment

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -119,9 +119,44 @@ const appointmentsAdmin = async (req, res) => {
 
     }
 
+}
+
+
+//API's Controller for appointment cancellation through Admin
+const appointmentCancel = async (req, res) => {
+
+    try {
+
+        const { appointmentId } = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+
+        // releasing doctor slot
+
+        const { docId, slotDate, slotTime } = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        const slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(slot => slot !== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, { slots_booked })
+
+        res.json({ success: true, message: `Appointment Cancelled for Time - ${slotTime}` })
+
+    }
+    catch (error) {
+
+        console.log("Error Occured while cancelling the slot time of the doctor. Error : ", error)
+        res.json({ success: false, message: error.message })
+
+    }
 
 }
 
 
 export default addDoctor
-export { loginAdmin, allDoctors, appointmentsAdmin }
+export { loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel }
